08137c1b71f58aa332f4f8fa823cbebd63a14c39
irtizak <irtizakaleem@gmail.com>
2021-05-25 19:50:28 -0400

    Fit rf model

--- train.py
+++ train.py
@@ -1,7 +1,24 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import mean_squared_error
+import numpy as np
 
 # Load data
 X, y = load_iris(return_X_y=True)
 
-X_train, y_train, X_test, y_test = train_test_split(X, y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=786)
+
+clf = RandomForestClassifier(random_state=786)
+
+clf.fit(X_train, y_train)
+
+y_preds = clf.predict(X_test)
+
+model_error = round(np.sqrt(mean_squared_error(y_preds, y_test)), 2)
+
+print(model_error)
+
+
+
+
