d3bd664efd7adc4a59edbf909aa430a5618cbd9b
irtizak <irtizakaleem@gmail.com>
2021-05-25 20:06:16 -0400

    Add requirements file

--- train.py
+++ train.py
@@ -7,16 +7,22 @@ import numpy as np
 # Load data
 X, y = load_iris(return_X_y=True)
 
+# Create train test splits
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=786)
 
+# Declare model
 clf = RandomForestClassifier(random_state=786)
 
+# Fit model
 clf.fit(X_train, y_train)
 
+# Make predictions
 y_preds = clf.predict(X_test)
 
+# Calculate metric
 model_error = round(np.sqrt(mean_squared_error(y_preds, y_test)), 2)
 
+# Print metric
 print(model_error)
 
 
